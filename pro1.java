339727cae7f94d6250c48d84b265acd077155d27
Shubhamchavekar <97620401+Shubhamchavekar@users.noreply.github.com>
2022-01-13 11:39:06 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,6 @@
+class pro1
+{
+public static void main(String args[])
+{System.out.println("Name=Shubham Chavekar \nMob No=7875205564 \nEmail=shubhamchavekar@gmail.com \nAge=23 \nCity=Ratnagiri");
+}
+}
